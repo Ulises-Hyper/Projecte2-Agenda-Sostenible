2f14f98052d3ec4c3f338a73c8d014cb66cf8331
Ulises-Hyper <ugil@cendrassos.net>
2024-10-30 15:45:02 +0100

    Doble head eliminado

--- src/views/index.php
+++ src/views/index.php
@@ -1,17 +1,8 @@
 <!DOCTYPE html>
 <html lang="es">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Agenda Figuerenca</title>
-</head>
-
 <body>
     <!DOCTYPE html>
     <html lang="es">
-
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -22,7 +13,6 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
         <link rel="stylesheet" href="../../public/src/css/style.css">
     </head>
-
     <body class="bg-custom-green-lightest">
         <!-- Barra superior con buscador -->
         <div class="bg-custom-green-darkest py-2">
