2bb6539560711f0d4111508d7ceba3c81248a634
Ulises-Hyper <ugil@cendrassos.net>
2024-11-02 20:51:32 +0100

    Conexión con emeset y base de datos

---
+++ src/ProjectContainer.php
@@ -0,0 +1,12 @@
+<?php
+
+class ProjectContainer extends \Emeset\Container {
+
+    public function __construct($config){
+        $this->sql = new Db($config);
+        $this->config = $config;
+    }
+    public function UrlsPDO(){
+        //return new UrlsPDO($this->sql->get());
+    }
+}
---
+++ src/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = [
+    "db" => [
+        "name" => "agenda",
+        "user" => "root",
+        "pass" => "12345",
+        "host" => "localhost",
+    ]
+];
