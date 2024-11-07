4520511839f13ba224cd3c7f6d7ecc9de899c84b
joelpages02 <jpages@cendrassos.net>
2024-11-07 16:34:20 +0100

    Editar models/Users para que sea igual

--- src/models/Users.php
+++ src/models/Users.php
@@ -22,58 +22,44 @@ class Users
      * @param string $description
      * @return void
      */
-    public function add($Nom, $Cognoms, $Data_naixement, $Adrea_carrer, $Adrea_numero, $Adrea_ciutat, $Adrea_codi_postal, $Ruta_resguard)
+
+    public function add($username, $surname, $name, $email, $role, $profile_img, $password)
     {
-        $query = "insert into Registre (Nom, Cognoms, Data_naixement, Adrea_carrer, Adrea_numero, Adrea_ciutat, Adrea_codi_postal, Ruta_resguard, Data_registre) values (:Nom, :Cognoms, :Data_naixement, :Adrea_carrer, :Adrea_numero, :Adrea_ciutat, :Adrea_codi_postal, :Ruta_resguard, :Data_registre);";
+        $query = "INSERT INTO users (username, surname, name, email, role, profile_img, password) VALUES (:username, :surname, :name, :email, :role, :profile_img, :password);";
         $stm = $this->sql->prepare($query);
-        $stm->execute([":Nom" => $Nom, ":Cognoms" => $Cognoms, ":Data_naixement" => $Data_naixement, ":Adrea_carrer" => $Adrea_carrer, ":Adrea_numero" => $Adrea_numero, ":Adrea_ciutat" => $Adrea_ciutat, ":Adrea_codi_postal" => $Adrea_codi_postal, ":Ruta_resguard" => $Ruta_resguard, ":Data_registre" => date('Y-m-d H:i:s')]);
+        $stm->execute([":username" => $username, ":surname" => $surname, ":name" => $name, ":email" => $email, ":role" => $role, ":profile_img" => $profile_img, ":password" => $password]);
 
         if ($stm->errorCode() !== '00000') {
             $err = $stm->errorInfo();
             $code = $stm->errorCode();
-            die("Error.   {$err[0]} - {$err[1]}\n{$err[2]} $query");
+            die("Error. {$err[0]} - {$err[1]}\n{$err[2]} $query");
         }
     }
 
-    /**
-     * return the list of urls
-     *
-     * @return array  ["title", "url", "description"]
-     */
-    public function list()
+    public function delete($id)
     {
-        $query = "select Nom, Cognoms,Data_naixement, Adrea_carrer, Adrea_numero, Adrea_ciutat, Adrea_codi_postal, Ruta_resguard, Data_registre from Registre;";
-        $urls = [];
-        foreach ($this->sql->query($query, \PDO::FETCH_ASSOC) as $url) {
-            $urls[$url["Ruta_resguard"]] = $url;
-        }
 
-        if ($this->sql->errorCode() !== '00000') {
-            $err = $this->sql->errorInfo();
-            $code = $this->sql->errorCode();
-            die("Error.   {$err[0]} - {$err[1]}\n{$err[2]} $query");
-        }
-        return $urls;
-    }
-
-    public function delete($id){
-        $query = "DELETE FROM users WHERE user_id = :user_id";
+        // Cambiar el nombre del parámetro en la consulta a :id para que coincida
+        $query = "DELETE FROM users WHERE user_id = :user_id";  // Asegúrate de usar el nombre correcto para el parámetro
         $stm = $this->sql->prepare($query);
-        $stm->execute([":user_id" => $id]);
+        $stm->execute([":user_id" => $id]);  // El parámetro debe coincidir con el de la consulta
+
+        // Manejo de errores
+        if ($stm->errorCode() !== '00000') {
+            $err = $stm->errorInfo();
+            die("Error al eliminar: {$err[0]} - {$err[1]}\n{$err[2]}");
+        }
     }
-
-
 
     public function getById($id)
     {
-        $query = "select id, title, url, description from links where id = :id";
+        $query = "select user_id, username, surname, name, email, role, profile_img from users where user_id = :user_id";
         $stm = $this->sql->prepare($query);
-        $stm->execute([":id" => $id]);
+        $stm->execute([":user_id" => $id]);
         $result = $stm->fetch(PDO::FETCH_ASSOC);
 
         return $result;
     }
-
 
     public function getAllUsers()
     {
@@ -84,10 +70,47 @@ class Users
         }
         return $results;
     }
+
+    // Función para hacer un update en el dashboard.
+    public function update($id, $user)
+    {
+        $query = "UPDATE users SET 
+            username = :username, 
+            surname = :surname, 
+            name = :name, 
+            email = :email, 
+            role = :role,
+            profile_img = :profile_img,
+            password = :password
+            WHERE user_id = :user_id";
+
+        $stmt = $this->sql->prepare($query);
+
+        // Ejecuta la consulta con los valores en el arreglo $data
+        $stmt->execute([
+            ':user_id' => $id,
+            ':username' => $user['username'],
+            ':surname' => $user['surname'],
+            ':name' => $user['name'],
+            ':email' => $user['email'],
+            ':role' => $user['role'],
+            ':profile_img' => $user['profile_img'],
+            ':password' => $user['password']
+        ]);
+
+        return $stmt->rowCount(); // Devuelve el número de filas afectadas
+    }
+    
+    public function getRegister($username, $surname, $name, $email, $role, $password){
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        $query = "INSERT INTO users (username, surname, name, email, role, password) VALUES (:username, :surname, :name, :email, :role, :password)";
+        $stm = $this->sql->prepare($query);
+        $stm->execute([":username" => $username, ":surname" => $surname, ":name" => $name, ":email" => $email, ":role" => $role, ":password" => $hashedPassword]);
+
+        if ($stm->errorCode() !== '00000') {
+            $err = $stm->errorInfo();
+            $code = $stm->errorCode();
+            die("Error. {$err[0]} - {$err[1]}\n{$err[2]} $query");
+        }
+    }
 }
-
-
-// foreach ($sql->query($query, PDO::FETCH_ASSOC) as $tasca) {
- //   print_r($tasca);
-// $tasques[] = $tasca["id"] . ", " . $tasca["tasca"];
-// }
